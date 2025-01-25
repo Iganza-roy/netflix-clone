56b4f0a6161b0e1a059f711bb757bc1b73beb128
Roy Iganza Kibata <iganzaroy55@gmail.com>
2025-01-25 22:33:41 +0300

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,13 +4,6 @@ import './Home.css';
 const Home = () => {
   return (
     <div className='home'>
-      <h1>Welcome to My Website!</h1>
-      <p>This is the home page.</p>
-      <p>Feel free to explore the other pages.</p>
-      <p>Created by: John Doe</p>
-      <p>Last updated: August 15, 2021</p>
-      <p>Version: 1.0.0</p>
-      <p>Copyright © 2021 My Website. All rights reserved.</p>
     </div>
   );
 };
