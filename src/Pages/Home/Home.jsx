068a4cba3cf2a3204236152f8bec9f80ea9b1c61
Iganza-roy <iganzaroy55@gmail.com>
2025-02-04 16:30:04 +0300

    fix: resolve merge conflict in Home component by restoring Navbar

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,10 +4,7 @@ import Navbar from '../../Components/Navbar/Navbar';
 const Home = () => {
   return (
     <div className='home'>
-<<<<<<< HEAD
       <Navbar />
-=======
->>>>>>> 519522ad205749ba0777a6e4901a0ec2db84b381
     </div>
   );
 };
