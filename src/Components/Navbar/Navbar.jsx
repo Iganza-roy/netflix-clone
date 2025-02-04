b16b555cc702e4bc5b45bf23f8b2b07d6846d720
Iganza-roy <iganzaroy55@gmail.com>
2025-02-04 11:27:33 +0300

    add Navbar component and integrate it into Home page; update App component to render Home

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,35 @@
-import React from 'react';
 import './Navbar.css';
+import logo from '../../assets/logo.png';
+import search_icon from '../../assets/search_icon.svg';
+import bell_icon from '../../assets/bell_icon.svg';
+import profile_img from '../../assets/profile_img.png';
+import caret_icon from '../../assets/caret_icon.svg';
 
 const Navbar = () => {
-  return <div className='navbar'></div>;
+  return (
+    <div className='navbar'>
+      <div className='navbar-left'>
+        <img src={logo} alt='logo' />
+        <ul>
+          <li>Home</li>
+          <li>TV shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My list</li>
+          <li>Browse by Language</li>
+        </ul>
+      </div>
+      <div className='navbar-right'>
+        <img src={search_icon} alt='search' className='icons' />
+        <p>Children</p>
+        <img src={bell_icon} alt='notification' className='icons' />
+        <div className='navbar-profile'>
+          <img src={profile_img} alt='profile' className='profile' />
+          <img src={caret_icon} alt='caret' />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
