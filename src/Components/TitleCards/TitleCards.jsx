e155c496a17957ea6beaf12597787f0ecdd301bb
Iganza-roy <iganzaroy55@gmail.com>
2025-02-17 22:33:19 +0300

    feat: add hero section with banner and title cards to Home page

--- src/Components/TitleCards/TitleCards.jsx
+++ src/Components/TitleCards/TitleCards.jsx
@@ -1,8 +1,36 @@
-import React from 'react';
 import './TitleCards.css';
+import cards_data from '../../assets/cards/Cards_data';
+import { useRef } from 'react';
+import { useEffect } from 'react';
 
 const TitleCards = () => {
-  return <div className='titlecards'></div>;
+  const cardsRef = useRef();
+
+  const handleWheel = (e) => {
+    e.preventDefault();
+
+    cardsRef.current.scrollLeft += e.deltaY;
+  };
+
+  useEffect(() => {
+    cardsRef.current.addEventListener('wheel', handleWheel);
+  }, []);
+
+  return (
+    <div className='title-cards'>
+      <h2>Popular on Netlix</h2>
+      <div className='card-list' ref={cardsRef}>
+        {cards_data.map((card, index) => {
+          return (
+            <div className='card' key={index}>
+              <img src={card.image} alt={card.name} />
+              <p>{card.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default TitleCards;
