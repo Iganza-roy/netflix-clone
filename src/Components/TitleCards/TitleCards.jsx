1df03a849cfcee9561fec2208cca78502bfdad09
Iganza-roy <iganzaroy55@gmail.com>
2025-02-18 14:07:50 +0300

    feat: implement Login page and routing, update TitleCards to fetch dynamic data

--- src/Components/TitleCards/TitleCards.jsx
+++ src/Components/TitleCards/TitleCards.jsx
@@ -1,10 +1,21 @@
 import './TitleCards.css';
 import cards_data from '../../assets/cards/Cards_data';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { useEffect } from 'react';
 
 const TitleCards = ({ title, category }) => {
   const cardsRef = useRef();
+
+  const [apiData, setApiData] = useState([]);
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMjBmYzNhODQxNGI3YmRhYzQ2M2NlNDBmYmU5NDQyZiIsIm5iZiI6MTczOTg3NTY3My41Niwic3ViIjoiNjdiNDY1NTk2MTEzYzQ5M2RjNmUwMDNkIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.qgxT1N26iQNb4-hFDY4eCryzfCZUJhfF9u7NF6RQnvc',
+    },
+  };
 
   const handleWheel = (e) => {
     e.preventDefault();
@@ -14,17 +25,30 @@ const TitleCards = ({ title, category }) => {
 
   useEffect(() => {
     cardsRef.current.addEventListener('wheel', handleWheel);
+
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        category ? category : 'now_playing'
+      }?language=en-US&page=1`,
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => setApiData(res.results))
+      .catch((err) => console.error(err));
   }, []);
 
   return (
     <div className='title-cards'>
       <h2>{title ? title : 'Popular on Netlix'}</h2>
       <div className='card-list' ref={cardsRef}>
-        {cards_data.map((card, index) => {
+        {apiData.map((card, index) => {
           return (
             <div className='card' key={index}>
-              <img src={card.image} alt={card.name} />
-              <p>{card.name}</p>
+              <img
+                src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path}
+                alt={card.original_title}
+              />
+              <p>{card.original_title}</p>
             </div>
           );
         })}
